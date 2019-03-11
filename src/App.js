a2b353b7035aea6b4f403dfcbaf0511aaa446e47
MPArchana <archana79168@gmail.com>
2019-03-11 11:57:09 +0530

    contact-app

--- src/App.js
+++ src/App.js
@@ -1,89 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button, Grid } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 class ContactApp extends Component {
   constructor(props){
     super(props);
-    this.emailChange = this.emailChange.bind(this);
-    this.nameChange = this.nameChange.bind(this);
-    this.mobileChange = this.mobileChange.bind(this);
+
+    this.titleChange = this.titleChange.bind(this);
+    this.bodyChange = this.bodyChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      name: '', mobile: '', email: '', items: []
-    };
+      error: null,
+      isLoaded: false, 
+      items: []
+    }
   }
-
-  emailChange(e){
+  titleChange(n){
     this.setState({
-      email: e.target.value
+      title: n.target.value
     })
   }
 
-  nameChange(n){
+  bodyChange(m){
     this.setState({
-      name: n.target.value
-    })
-  }
-
-  mobileChange(m){
-    this.setState({
-      mobile: m.target.value
+      body: m.target.value
     })
   }
 
   handleSubmit(e){
     e.preventDefault();
     const newItem = {
-      name: this.state.name,
-      mobile: this.state.mobile,
-      email: this.state.email
+      title: this.state.title,
+      body: this.state.body,
     };
-    this.setState(state => ({
-      items: state.items.concat(newItem)
-    }));
+    this.createPost(newItem);
   }
 
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+  
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  createPost(data) {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }}).then(response => {
+        alert(response.statusText);
+        return response;
+      });
+    }
+
   render() {
-      let name;
-      return(
-        <div>
-          <h1>Contacts Application</h1>
-          <hr />
-          <ul>
-            {this.state.items.map(item => (
-              <li key={item.email}>{item.name}, {item.mobile}, {item.email}</li>
-            ))}
-          </ul>
-          <div>
-            <h3>Add Contact Form</h3>
-            <form onSubmit={this.handleSubmit}>
-            <div style={{ padding: 10 }}>
-            <Grid container spacing={24}>            
-              <Grid item xs={6}>
-                <span>Name:</span>
-                <input type="text" id="name" onChange={this.nameChange} />
-              </Grid>
-            </Grid>
-            </div>
-            <div style={{ padding: 10 }}>
-              <span>Mobile No.:</span>
-              <input type="text" id="mobile" onChange={this.mobileChange} />
-            </div>
-            <div style={{ padding: 10 }}>
-              <span>Email ID:</span>
-              <input type="text" id="email-id" onChange={this.emailChange} />
-            </div>
-            <div style={{ padding: 20 }}>
-              <Button variant="contained" color="primary" type="submit" >Submit</Button>
-            </div>
-            </form>
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className= "container">
+          <form onSubmit={this.handleSubmit}>
+          <div style={{ padding: 10 }}>
+            <span>Title.:</span>
+            <input type="text" id="mobile" onChange={this.titleChange} />
           </div>
+          <div style={{ padding: 10 }}>
+            <span>Body:</span>
+            <input type="text" id="email-id" onChange={this.bodyChange} />
+          </div>
+          <div style={{ padding: 20 }}>
+            <Button variant="contained" color="primary" type="submit" >Submit</Button>
+          </div>
+          </form>
+
+          {items.map(result => (
+            <div className= "row">
+              <div className= "col-sm">
+              {result.id}
+              </div>
+              <div className= "col-sm">
+              {result.name}
+              </div>
+              <div className= "col-sm">
+              {result.username}
+              </div>
+              <div className= "col-sm">
+              {result.address.city}
+              </div>
+              <div className= "col-sm">
+              {result.phone}
+              </div>
+              <div className= "col-sm">
+              {result.website}
+              </div>
+              <div className= "col-sm">
+              {result.company.name}
+              </div>
+              <div className= "col-sm">
+              {result.company.catchPhrase}
+              </div>
+              <div className= "col-sm">
+              {result.company.bs}
+              </div>
+            </div>
+          ))}
         </div>
       );
     }
   }
+} 
 
   export default ContactApp;
